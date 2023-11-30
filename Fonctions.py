e893f6fefa1c93d1793b3f8e49e0c9925bf56da2
Emma-924 <151037998+Emma-924@users.noreply.github.com>
2023-11-30 14:06:33 +0100

    Update Fonctions.py

--- Fonctions.py
+++ Fonctions.py
@@ -1,27 +1,34 @@
 import math
 import os
 
+president_dict = {
+    "Chirac": "Jacques",
+    "Giscard dEstaing": "Valéry",
+    "Hollande": "François",
+    "Macron": "Emmanuel",
+    "Mitterrand": "François",
+    "Sarkozy": "Nicolas"}
+
 def list_of_files(directory, extension):
     files_names = []
-    for filename in os.listdir(directory):
+    file_list = os.listdir(directory)
+    for filename in file_list:
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
-
 directory = "speeches"
 files_names = list_of_files(directory, "txt")
-def tf(f):
-    contenu = f.read()
-    mots = contenu.split()
-    compteur_mots = {}
-
-    for mot in mots:
-        if mot in compteur_mots:
-            compteur_mots[mot] += 1
-        else:
-            compteur_mots[mot] = 1
-
-    return compteur_mots
+def tf(file_path):
+    with open(file_path, "r", encoding='utf-8') as file:
+        #print("=$$$$=", file_path)
+        words = file.read().split()
+        word_count = {}
+        for word in words:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+    return word_count
 
 def idf(directory):
     term_count = {}
@@ -52,68 +59,74 @@ def tfidf(directory):
     return tfidf_matrix
 
 def mots_chirac():
-    with open(f"cleaned/Nomination_Chirac1.txt", "r", encoding='utf-8', errors='ignore') as f:
-        d1=tf(f)
-    with open(f"cleaned/Nomination_Chirac2.txt", "r", encoding='utf-8', errors='ignore') as f:
-        d2=tf(f)
-    d=d1
+    d1=tf('cleaned/Nomination_Chirac1.txt')
+    d2=tf('cleaned/Nomination_Chirac2.txt')
     for i in d2:
-        if i not in d :
-            d[i] = d2[i]
-        else :
-            d[i] = d[i] + d2[i]
+        if i not in d1 : d1[i] = d2[i]
+        else : d1[i] = d1[i] + d2[i]
     L=[]
-    for i in d.items() :
-        L.append(i[1])
+    for i in d1.items() : L.append(i[1])
     maxi = max(L)
-    for i in d :
-        if d[i] == maxi:
-            return i
+    for i in d1 :
+        if d1[i] == maxi : return i
+
+def score_tfidf_max():
+    L = []
+    for i in range(len(tfidf("cleaned"))):
+        for j in tfidf("cleaned")[i].items() : L.append(j[1])
+    maxi = max(L)
+    for i in range(len(tfidf("cleaned"))):
+        for j in tfidf("cleaned")[i] :
+            if tfidf("cleaned")[i][j]== maxi : return j
 
 def mots_non_importants():
     mots_doc = set()
     for value in files_names:
         with open(f"cleaned/{value}", "r", encoding='utf-8', errors='ignore') as f:
             mot = f.read().split()
-            for mots in mot:
-                mots_doc.add(mots)
-
+            for mots in mot : mots_doc.add(mots)
     m_non_importants = []
     for mot in mots_doc:
-        if idf(files_names)[mot] == 0:
-            m_non_importants.append(mot)
+        if idf(files_names)[mot] == 0: m_non_importants.append(mot)
     return m_non_importants
+
+
+files_list = list_of_files('cleaned', "txt")
 
 def pres_nation():
     L = []
-    for value in files_names:
-        with open(f"cleaned/{value}", "r", encoding='utf-8', errors='ignore') as f:
-            if 'nation' in tf(f): L.append(value)
+    for file in files_list:
+        if 'nation' in tf(f'cleaned/{file}'): L.append(file)
     pres = []
     for i in L:
-        '''a = i.split('.')[0].split('_')[1]
-        if '1' in a or '2' in a:
-            a = a[:-1]
-        if a not in pres : pres.append(a)'''
-        pres.append(i.split('.')[0].split('_')[1])
-    return pres
+        nom = i.split('.')[0].split('_')[1]
+        if ('1' in i or '2' in i ):
+            for j in president_dict :
+                prénom = president_dict[nom[:-1]]
+                pres.append(prénom + ' ' + nom[:-1])
+        else:
+            for j in president_dict :
+                prénom = president_dict[nom]
+                pres.append(prénom + ' ' + nom)
+    return list(set(pres))
 
 
 def pres_nation_max():
     d = {}
     pres = pres_nation()
-    for value in pres:
-        with open(f"cleaned/Nomination_{value}.txt", "r", encoding='utf-8', errors='ignore') as f:
-            d1 = tf(f)
-            d[value] = d1['nation']
-
-    # président qui en parle le plus
+    for file in files_list:
+        d1 = tf(f'cleaned/{file}')
+        d[file]=0
+        if 'nation' in d1:
+            d[file] = d1['nation']
     max = 0
     for i in d:
-        if d[i] > max:
-            max = d[i]
+        if d[i] > max: max = d[i]
     for i in d:
         if d[i] == max:
-            if '1' in i or '2' in i :
-                return i[:-1]
+            if '1' in i or '2' in i : return (i.split('.')[0].split('_')[1])[:-1]
+            else : return i.split('.')[0].split('_')[1]
 
+def pres_climat():
+    for file in files_list:
+        if 'climat' in tf(f'cleaned/{file}') or 'écologie' in tf(f'cleaned/{file}'): return file.split('.')[0].split('_')[1]
